4385104a49439ec7997e7e09a74f235644278bdb
UnKevined <91185399+UnKevined@users.noreply.github.com>
2024-01-04 11:40:26 +0100

    Update main.js

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// main.js
+
+// Function to toggle the navigation dropdown
+function toggleNav() {
+  var navDropdown = document.querySelector('.nav-dropdown');
+  navDropdown.classList.toggle('show');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Selecting elements for menu icon and navigation dropdown
+  var menuIcon = document.querySelector('.menu-icon');
+  var navDropdown = document.querySelector('.nav-dropdown');
+
+  // Event listener for clicking the menu icon to toggle the dropdown
+  menuIcon.addEventListener('click', function () {
+    navDropdown.classList.toggle('show');
+  });
+
+  // Event listeners for dropdown links to close the dropdown
+  var navLinks = document.querySelectorAll('.nav-dropdown a');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      navDropdown.classList.remove('show');
+    });
+  });
+});
